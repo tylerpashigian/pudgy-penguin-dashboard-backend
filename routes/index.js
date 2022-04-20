6f138a45c1d9afaa0eb04fc9c0fb445950c0c9ec
Tyler Pashigian <tylerpashigian@gmail.com>
2022-04-19 22:01:02 -0400

    Removing all views and rendering engines
    - making heartbeat the default endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/heartbeat', function (req, res, next) {
+  res.send('Server is up and runniing');
 });
 
 module.exports = router;
